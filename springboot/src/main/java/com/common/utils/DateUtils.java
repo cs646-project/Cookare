e486122b1b5bddf242df866136c9db0084041901
Chengzheng Zhan <c8zhan@uwaterloo.ca>
2022-06-21 17:10:22 -0400

    Zachariah: login

--- springboot/src/main/java/com/common/utils/DateUtils.java
+++ springboot/src/main/java/com/common/utils/DateUtils.java
@@ -1,7 +1,6 @@
 package com.common.utils;
 
 import java.time.*;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateUtils {
@@ -12,16 +11,6 @@ public class DateUtils {
      */
     public static LocalDateTime now() {
         return LocalDateTime.now();
-    }
-
-    /**
-     * 基于当前时区，获取当前日期、时间，并按指定的Pattern格式化为字符串
-     *
-     * @param pattern 格式
-     * @return 字符串
-     */
-    public static String current(String pattern) {
-        return format(now(), pattern);
     }
 
     /**
@@ -41,62 +30,5 @@ public class DateUtils {
      */
     public static Date currentDate() {
         return localDateTime2Date(now());
-    }
-
-    /**
-     * 基于当前时区，获取当前日期
-     *
-     * @return 日期
-     */
-    public static LocalDate getDate() {
-        return LocalDate.now();
-    }
-
-    /**
-     * 基于当前时区，获取当前时间
-     *
-     * @return 时间
-     */
-    public static LocalTime getTime() {
-        return LocalTime.now();
-    }
-
-    public static int getYear() {
-        return now().getYear();
-    }
-    public static int getMonth() {
-        return now().getMonthValue();
-    }
-    public static int getDay() {
-        return now().getDayOfMonth();
-    }
-    ////////////// 日期格式化操作 日期 -> 字符串 ///////////////////
-
-    /**
-     * 格式化日期为字符串
-     *
-     * @param localDateTime 时刻
-     * @param pattern       格式
-     * @return 日期字符串
-     */
-    public static String format(LocalDateTime localDateTime, String pattern) {
-
-        return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
-    }
-
-    /**
-     * 格式化日期为字符串
-     *
-     * @param date    date
-     * @param pattern 格式
-     * @return 日期字符串
-     */
-    public static String format(Date date, String pattern) {
-
-        Instant instant = date.toInstant();
-
-        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-
-        return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
     }
 }
