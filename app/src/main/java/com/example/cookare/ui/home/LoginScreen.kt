ea0867605e3263bf354263020db4e434f5118d63
Hongyi Shi <h235shi@uwaterloo.ca>
2022-07-10 04:36:24 -0400

    fix LoginScreen.kt

--- app/src/main/java/com/example/cookare/ui/home/LoginScreen.kt
+++ app/src/main/java/com/example/cookare/ui/home/LoginScreen.kt
@@ -1,13 +1,11 @@
 package com.example.cookare.ui.home
 
 import android.annotation.SuppressLint
-import android.content.Context
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
@@ -16,8 +14,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -27,12 +23,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.*
 import androidx.compose.ui.unit.dp
@@ -41,12 +32,8 @@ import com.guru.fontawesomecomposelib.FaIcon
 import com.example.cookare.ui.theme.TextFieldDefaultsMaterial
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import com.example.cookare.ui.home.HomeScreen
-import com.example.cookare.BottomNavigationContent
-import com.example.cookare.HomeScreenNavigate
+import com.example.cookare.ui.HomeScreenNavigate
 import com.example.cookare.R
-import com.example.cookare.ui.utils.TestTags
-
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -283,11 +270,10 @@ fun LoginOnboarding() {
 //    var loggedIn by remember { mutableStateOf(false) }
     val userStateVM = UserState.current
     val coroutineScope = rememberCoroutineScope()
-    val homeScreenState = rememberSaveable { mutableStateOf(BottomNavType.HOME) }
     Crossfade(targetState = userStateVM.isLoggedIn) {
         if (userStateVM.isLoggedIn) {
             // Text(text = "in LoginOnboaring function")
-            HomeScreenNavigate(homeScreenState)
+            HomeScreenNavigate()
         } else {
             LoginScreen {
                 coroutineScope.launch {
